a804d4764ebc85493c09669a7cf5fbe5aae30b29
KangMinGyo <kang15567@kakao.com>
2023-08-02 21:47:05 +0900

    feat: 현재 버전 표시하는 기능 추가

--- MovieReview/Views/Setting/SettingViewController.swift
+++ MovieReview/Views/Setting/SettingViewController.swift
@@ -40,12 +40,21 @@ class SettingViewController: UIViewController {
         return view
     }()
     
+    let currentVersionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "현재 버전"
+        label.textColor = .gray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     //MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
+        currentVersionLabel.text = "현재 버전 : \(getCurrentVersion())"
         configureComponent()
     }
     
@@ -55,6 +64,7 @@ class SettingViewController: UIViewController {
         view.addSubview(topLineView)
         view.addSubview(feedbackButton)
         view.addSubview(buttomLineView)
+        view.addSubview(currentVersionLabel)
         
         NSLayoutConstraint.activate([
             topLineView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
@@ -70,6 +80,10 @@ class SettingViewController: UIViewController {
             buttomLineView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             buttomLineView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             buttomLineView.heightAnchor.constraint(equalToConstant: 1),
+
+//            currentVersionLabel.topAnchor.constraint(equalTo: buttomLineView.bottomAnchor, constant: 10),
+            currentVersionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            currentVersionLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40)
         ])
     }
 }
